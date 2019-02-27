5f64d5c8ff727f40207792afff7837f1e95af89c
Nikolay Bikchentaev <ironhead749@gmail.com>
2019-02-27 15:33:57 +0200

    Carried out a bit of code refactoring writing a "giveProductContainer" function

--- scripts.js
+++ scripts.js
@@ -26,16 +26,16 @@ $(function () {
 
     $prod_list.on("click", "div.product .buy-button", function (e) {
         var $button_clicked = $("button#" + e.target.id);
-        var $parent = $button_clicked.closest("div");
+        var $container = giveProductContainer($button_clicked);
 
         if($button_clicked.attr("clicked").localeCompare("false") == 0) {
             $("span#" + e.target.id).css("text-decoration", "line-through");
 
             moveToBought(e.target.id);
 
-            $parent.find(".dismiss-button").css("display", "none");
-            $parent.find(".plus-button").css("visibility", "hidden");
-            $parent.find(".minus-button").css("visibility", "hidden");
+            $container.find(".dismiss-button").css("display", "none");
+            $container.find(".plus-button").css("visibility", "hidden");
+            $container.find(".minus-button").css("visibility", "hidden");
 
             $button_clicked.text("Не куплено");
             $button_clicked.css("width", "70%");
@@ -46,9 +46,9 @@ $(function () {
 
             moveToRemained(e.target.id);
 
-            $parent.find(".dismiss-button").css("display", "initial");
-            $parent.find(".plus-button").css("visibility", "initial");
-            $parent.find(".minus-button").css("visibility", "initial");
+            $container.find(".dismiss-button").css("display", "initial");
+            $container.find(".plus-button").css("visibility", "initial");
+            $container.find(".minus-button").css("visibility", "initial");
 
             $button_clicked.text("Куплено");
             $button_clicked.css("width", "initial");
@@ -59,33 +59,35 @@ $(function () {
 
     $prod_list.on("click", "div.product .dismiss-button", function (e) {
         var $button_clicked = $("button#" + e.target.id);
-        var id = $button_clicked.closest("div").find(".prod-name").attr("id");
+        var $container = giveProductContainer($button_clicked);
+        var id = $container.find(".prod-name").attr("id");
         $remained_prod_list.find("#status-" + id).remove();
-        $button_clicked.closest("div").remove();
+        $container.remove();
     });
 
     $prod_list.on("click", "div.product .plus-button", function (e) {
-        var $parent = $("button#" + e.target.id).closest("div");
-        var quantity = parseInt($parent.find(".quantity").text(), 10) + 1;
-        $parent.find(".quantity").text(quantity);
-        $parent.find(".minus-button").attr("active", "true");
-        $parent.find(".minus-button").css("opacity", "1");
+        var $container = giveProductContainer($("button#" + e.target.id));
+        var quantity = parseInt($container.find(".quantity").text(), 10) + 1;
+        $container.find(".quantity").text(quantity);
+        $container.find(".minus-button").attr("active", "true");
+        $container.find(".minus-button").css("opacity", "1");
 
-        var id = $parent.find(".prod-name").attr("id");
+        var id = $container.find(".prod-name").attr("id");
         $remained_prod_list.find("#status-" + id).find(".quantity").text(quantity);
     })
 
     $prod_list.on("click", "div.product .minus-button", function (e) {
         var $button_clicked = $("button#" + e.target.id);
-        var $parent = $button_clicked.closest("div");
+        var $container = giveProductContainer($button_clicked);
+
         if($button_clicked.attr("active").localeCompare("true") == 0){
-            var id = $parent.find(".prod-name").attr("id");
-            var quantity = parseInt($parent.find(".quantity").text(), 10) - 1;
+            var id = $container.find(".prod-name").attr("id");
+            var quantity = parseInt($container.find(".quantity").text(), 10) - 1;
             if(quantity > 1){
-                $parent.find(".quantity").text(quantity);
+                $container.find(".quantity").text(quantity);
                 $remained_prod_list.find("#status-" + id).find(".quantity").text(quantity);
             } else {
-                $parent.find(".quantity").text(quantity);
+                $container.find(".quantity").text(quantity);
                 $remained_prod_list.find("#status-" + id).find(".quantity").text(quantity);
                 $button_clicked.attr("active", "false");
                 $button_clicked.css("opacity", ".45");
@@ -118,21 +120,6 @@ $(function () {
         }
     });
 });
-
-function moveToRemained(prod_id) {
-    var $prod = $("#bought-products-list").find("#status-" + prod_id).clone();
-    $("#bought-products-list").find("#status-" + prod_id).remove();
-    $prod.css("text-decoration", "none");
-    $("#remained-products-list").append($prod);
-}
-
-
-function moveToBought(prod_id) {
-    var $prod = $("#remained-products-list").find("#status-" + prod_id).clone();
-    $("#remained-products-list").find("#status-" + prod_id).remove();
-    $prod.css("text-decoration", "line-through");
-    $("#bought-products-list").append($prod);
-}
 
 function initBuyList() {
     addProduct("Помідори");
@@ -171,4 +158,23 @@ function addProduct_SL(product_name){
     $("#remained-products-list").append($sl_product_template);
 
     counter++;
+}
+
+function moveToRemained(prod_id) {
+    var $prod = $("#bought-products-list").find("#status-" + prod_id).clone();
+    $("#bought-products-list").find("#status-" + prod_id).remove();
+    $prod.css("text-decoration", "none");
+    $("#remained-products-list").append($prod);
+}
+
+
+function moveToBought(prod_id) {
+    var $prod = $("#remained-products-list").find("#status-" + prod_id).clone();
+    $("#remained-products-list").find("#status-" + prod_id).remove();
+    $prod.css("text-decoration", "line-through");
+    $("#bought-products-list").append($prod);
+}
+
+function giveProductContainer($current) {
+    return $current.closest("div");
 }
